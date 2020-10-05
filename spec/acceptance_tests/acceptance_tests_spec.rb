a5e16a321b81aba8fc7b03b393af98901e0c9c4e
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-10-05 09:39:11 +0100

    Acceptance test user makes first move

--- spec/acceptance_tests/acceptance_tests_spec.rb
+++ spec/acceptance_tests/acceptance_tests_spec.rb
@@ -9,10 +9,29 @@ RSpec.describe 'the class-based user interface' do
 
         # Assert
         expect(game.grid).to eq([
-                                  [nil, nil, nil],
-                                  [nil, nil, nil],
-                                  [nil, nil, nil]
+                                  [nil, nil, nil], # 3
+                                  [nil, nil, nil], # 2
+                                  [nil, nil, nil]  # 1
+                                  # A    B    C
                                 ])
+      end
+
+      context 'when the user makes her first move' do
+        it 'records that move on the grid' do
+          # Arrange
+          game = GameManager.new
+          game.new_player('X')
+
+          # Act
+          game.add_move('X', 'A3')
+
+          # Assert
+          expect(game.grid).to eq([
+                                    ['X', nil, nil],
+                                    [nil, nil, nil],
+                                    [nil, nil, nil]
+                                  ])
+        end
       end
     end
   end
