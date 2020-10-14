0991e528a5cc639422ba548a33d478c5c89d3418
Ben <ben.dalton@madetech.com>
2020-10-14 16:00:25 +0100

    added some ideas for AI player in ai_player.rb

---
+++ lib/ai_player.rb
@@ -0,0 +1,21 @@
+# class AIPlayer
+#     def initialize(game: GameManager.new, grid: Grid.new)
+#         @game = game
+#     end
+
+#     def take_turn(random_coords)
+#     end
+
+#     def random_coords(@grid)
+#         x_axis = ["A", "B", "C"] 
+#         y_axis = ["1", "2", "3"]
+
+#         coords = "#{x_axis.random}#{y_axis.random}"
+        
+#     end
+
+#     def validate_ai_move(random_coords)
+#         @grid.validate_move(random_coords)
+        
+#     end
+# end
