7e0150a9bfbee3b87c8b54aa7f443ae96893a675
Ben <ben.dalton@madetech.com>
2020-10-20 11:09:57 +0100

    CLIStrategy displays empty grid to command line test

--- lib/cli_strategy.rb
+++ lib/cli_strategy.rb
@@ -1,3 +1,15 @@
 class CLIStrategy
-  def get_move(grid); end
+  def get_move(grid)
+    p <<~GRID
+    +---+---+---+
+    | 0 | 1 | 2 |
+    +---+---+---+
+    | 3 | 4 | 5 |   
+    +---+---+---+
+    | 6 | 7 | 8 |
+    +---+---+---+
+    GRID
+  end
 end
+
+
--- spec/cli_strategy_spec.rb
+++ spec/cli_strategy_spec.rb
@@ -13,9 +13,54 @@ RSpec.describe CLIStrategy do
 
   context 'when passed an empty grid' do
     it 'displays the grid to the command line' do
+      # Arrange
+      grid = double('grid', raw: [0, 1, 2, 3, 4, 5, 6, 7, 8], empty_squares: [0, 1, 2, 3, 4, 5, 6, 7, 8])
+      strategy = CLIStrategy.new
+      expected_output = <<~GRID
+          +---+---+---+
+          | 0 | 1 | 2 |
+          +---+---+---+
+          | 3 | 4 | 5 |   
+          +---+---+---+
+          | 6 | 7 | 8 |
+          +---+---+---+
+      GRID
+
+      # Act + Assert
+      expect { strategy.get_move(grid) }.to output(expected_output).to_stdout
     end
 
+  context 'when passed a partially filled grid' do
+    it 'displays the grid to the command line' do
+      # Arrange
+      grid = double('grid', raw: ['X', 1, 2, 'X', 4, 5, 'O', 7, 8], empty_squares: [1, 2, 4, 5, 7, 8])
+      strategy = CLIStrategy.new
+      expected_output = <<~GRID
+          +---+---+---+
+          | X | 1 | 2 |
+          +---+---+---+
+          | X | 4 | 5 |   
+          +---+---+---+
+          | O | 7 | 8 |
+          +---+---+---+
+      GRID
+
+      # Act + Assert
+      expect { strategy.get_move(grid) }.to output(expected_output).to_stdout
+    end
+  end
+
     xit 'requests the next move from the user' do
+            # Arrange
+            grid = double('grid', raw: [0, 1, 2, 3, 4, 5, 6, 7, 8], empty_sqaures: [0, 1, 2, 3, 4, 5, 6, 7, 8])
+            strategy = CLIStrategy.new
+            allow($stdin).to receive(:gets).and_return(0)
+            
+            # Act
+            move = strategy.get_move(grid)
+            
+            # Assert
+            expect(move).to eq(0)
     end
 
     xit 'does not allow the user to enter an invalid move' do
